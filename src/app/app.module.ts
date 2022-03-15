1583e8a0fcf5b792ff4089068dede696354b0757
Bastos07 <94012936+Bastos07@users.noreply.github.com>
2022-03-15 16:50:42 -0300

    150322

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,36 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+<<<<<<< Updated upstream
+=======
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
+<<<<<<< Updated upstream
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+=======
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
+>>>>>>> Stashed changes
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
